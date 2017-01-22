06ae84db2e856e8cc9137fb3a6739cebff568303
Giang Bui <giangbui@users.noreply.github.com>
2017-01-22 02:01:26 -0600

    add solution

--- Predict_the_Winner.cpp
+++ Predict_the_Winner.cpp
@@ -7,3 +7,27 @@
 
 //DP formula:
 //dp(i, j) = max(sum(i, j-1) - dp(i, j-1) + nums[j], sum(i+1, j) - dp(i+1, j) + nums[i])
+
+class Solution {
+public:
+    bool PredictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return false;
+        vector<int> sums(n+1);
+        vector<vector<int>> dp(n, vector<int>(n));
+        for (int i = 0; i < n; i++) {
+            sums[i+1] = nums[i] + sums[i];
+        }
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j+i < n; j++) {
+                int sum1 = sums[j+i] - sums[j] - dp[j][j+i-1] + nums[j+i];
+                int sum2 = sums[j+i+1] - sums[j+1] - dp[j+1][j+i] + nums[j];
+                dp[j][j+i] = max(sum1, sum2);
+            }
+        }
+        return dp[0][n-1] >= sums[n] / 2 + sums[n] % 2;
+    }
+};
