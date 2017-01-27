d606bed4a9ebc4def7dcc4176b09e0f4e758bd97
Giang Bui <giangbui@users.noreply.github.com>
2017-01-27 16:18:22 -0600

    fix typos.

--- Kth_Largest_Element.cpp
+++ Kth_Largest_Element.cpp
@@ -2,7 +2,8 @@
 //Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
 //For example,
 //Given [3,2,1,5,6,4] and k = 2, return 5.
-//Solution: Using max_priority_queue give us the linear complexity. 
+//Solution: Using max_priority_queue gives us the linear complexity. Initialize an max priority_queue to store all the elements of the array
+// to get the k-th largest number, we loop the pop operator k times
 
 class Solution {
 public:
