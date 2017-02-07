796966e6bb966fcfb55ebba47209407fca423304
Giang Bui <giangbui@users.noreply.github.com>
2017-02-06 19:12:28 -0600

    initial commit

---
+++ Flatten_Binary_Tree.cpp
@@ -0,0 +1,21 @@
+
+Given a binary tree, flatten it to a linked list in-place.
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode *head = nullptr;
+        flattenWorker(root, head);
+    }
+private:
+    void flattenWorker(TreeNode* root, TreeNode*& head) {
+        if (root == nullptr) {
+            return;
+        }
+        flattenWorker(root->right, head);
+        flattenWorker(root->left, head);
+        root->left = nullptr;
+        root->right = head;
+        head = root;
+    }
+};
